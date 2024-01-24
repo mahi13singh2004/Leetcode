9a3715dfda8e4cff696833c6214d3a35c435e130
mahi13singh2004 <mahi13singh2004@gmail.com>
2024-01-24 18:56:29 +0530

    Create Rotatearray(189).cpp

---
+++ Arrays/Rotatearray(189).cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& array, int K) {
+        int N=array.size();
+        K=K%N;
+
+        int temp[1000001];
+
+        if(K==0){
+            for(int i=0; i<N; i++){
+                cout<<array[i]<<" ";
+            }
+        }
+
+        for(int i=N-K; i<N; i++){
+            temp[i-(N-K)]=array[i];
+        }
+
+        for(int i=N-K-1; i>=0; i--){
+            array[i+K]=array[i];
+        }
+
+        for(int i=0; i<K; i++){
+            array[i]=temp[i];
+        }
+        
+        for(int i=0; i<N; i++){
+            cout<<array[i]<<" ";
+        }
+    }
+};
