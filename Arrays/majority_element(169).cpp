2fb1327f0109f9882c7ea7aa7b2e9c74bc0ed77c
Mahi Singh <154801399+mahi13singh2004@users.noreply.github.com>
2024-03-08 17:08:58 +0530

    Create majority_element(169).cpp

---
+++ Arrays/majority_element(169).cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int element;
+        int count=0;
+
+        for(int i=0; i<nums.size(); i++){
+            if(count==0){
+                element=nums[i];
+                count++;
+            }
+            else if(nums[i]==element){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+
+        int count2=0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==element){
+                count2++;
+            }
+        }
+        
+        return element;
+    }
+};
